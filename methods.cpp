b489788e3cccef5bbcfb8fe1c7e2a2c2a3d920b5
caiofov <cfoviana@gmail.com>
2021-12-19 23:54:26 -0300

    gauss elimination

---
+++ methods.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+#include "geral.h"
+#include <vector>
+
+
+//A = matriz triangular superior
+//b=vetor constante
+
+vector<float> eliminacaoGauss(vector<vector<float>> A, vector<float> b){
+  int n = A.size(); //ordem de A
+
+  for(int k = 0; k < n-1; k++){ //para cada etapa K
+    printf("\nEtapa %d",k+1);
+    
+    for(int i = k+1; i < n; i++){ //para cada linha i
+      //fator m
+      float m = - A[i][k] / A[k][k];
+    
+      //atualizar a linha i da matriz, pecorrendo todas as colunas J
+      for(int j = k+1; j < n; j++){ //para cada coluna j
+        A[i][j] = m * A[k][j] + A[i][j];
+      }
+      printf("\nFator m: %f", m);
+      b[i] = m * b[k] + b[i]; //atualiza o vetor B na linha i
+
+      //zerar o elemento A[i][k]
+      A[i][k] = 0;
+      mostrarVetor(b);
+      mostrarVetor(A);
+    }}
+  printf("\n");
+  return substituicoes_retroativas(A,b);
+}
+
+bool escolhe_pivot(int k, vector<vector<float>> A, vector<float> b){
+  int n = A.size(), linha_pivot = k;
+  
+  bool houve_troca = false; //indica se houve troca de linha ao final da função
+  
+  float pivot = A[k][k]; //setta o primeiro pivot
+  
+  //indenfica o elemento de maior valor absoluto e a linha onde ele está
+  for(int i = k+1; i < n; i++){
+    
+    if( abs(A[i][k]) > abs(pivot) ){ //achou um novo pivot, então deve atualizar as variáveis utilizadas
+      pivot = A[i][k];
+      linha_pivot = i;
+    }
+  }
+
+  if(k!=linha_pivot){ //se a linha inicial que "entrou" na função for diferente da linha do pivot que foi encontrado, indica que deverá haver uma troca
+    A[k],A[linha_pivot] = A[linha_pivot], A[k];
+    b[k],b[linha_pivot] = b[linha_pivot], b[k];
+    
+    houve_troca = true;
+  }
+  return houve_troca;
+}
+vector<float> gaussParcial(vector<vector<float>> A, vector<float> b){
+  int n = A.size(); //ordem de A
+
+  for(int k = 0; k < n-1; k++){ //para cada etapa K
+    printf("\nEtapa %d",k+1);
+    
+    for(int i = k+1; i < n; i++){ //para cada linha i
+    //escolhe o pivot
+    escolhe_pivot(k,A,b);
+    //fator m
+    float m = - A[i][k] / A[k][k];
+    
+    //atualizar a linha i da matriz, pecorrendo todas as colunas J
+    for(int j = k+1; j < n; j++){ //para cada coluna j
+      A[i][j] = m * A[k][j] + A[i][j];
+    }
+
+    printf("\nFator m: %f", m);
+    b[i] = m * b[k] + b[i]; //atualiza o vetor B na linha i
+
+    //zerar o elemento A[i][k]
+    A[i][k] = 0;
+    mostrarVetor(b);
+    mostrarVetor(A);
+  }}
+  printf("\n");
+  return substituicoes_retroativas(A,b);
+}
